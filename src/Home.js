e2b0f72a9b177fb475b69b078d61e3d96eefcd5b
anishmhrzn47 <anishamhrzn47@gmail.com>
2021-08-31 13:22:44 +1000

    handle fetch errors

--- src/Home.js
+++ src/Home.js
@@ -7,6 +7,7 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
     const [isPending, ssetIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
 
     //define here to interact with data directly and pass as props
@@ -21,22 +22,34 @@ const Home = () => {
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json()
-            })
-            .then((data) => {
-                // console.log(data);
-                setBlogs(data);
-                ssetIsPending(false);
-            });
+                .then(res => {
+                    if(!res.ok){
+                       throw Error('Could not fetch data for that resource'); 
+                    }
+                    return res.json()
+                })
+                .then((data) => {
+                    // console.log(data);
+                    setBlogs(data);
+                    ssetIsPending(false);
+                    setError(null);
+                })
+                //catch() catches any kind of network error and fires a function 
+                .catch(err => {
+                    ssetIsPending(false);
+                   setError(err.message);
+                })
         }, 1000);
     }, []);
 
     return ( 
         <div className="home">
+
             {/* conditional templating  */}
+            { error && <div>{error}</div>}
             { isPending && <div>Loading...</div> }
             {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+        
         </div>
      );
 }
