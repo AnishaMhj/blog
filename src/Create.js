1ce1c57c532a7bbea058fb5b0d6a407775dc3165
anishmhrzn47 <anishamhrzn47@gmail.com>
2021-09-02 15:07:35 +1000

    make a POST Request and add new blog

--- src/Create.js
+++ src/Create.js
@@ -4,38 +4,60 @@ import { useState } from 'react';
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
-    const [author, setAuthor] = useState('Mario');
+    const [author, setAuthor] = useState('Anisha');
+    const [isPending, setIsPending] = useState(false);
     
     const handleSubmit= (e) =>{
         e.preventDefault(); //prevent page from refresh
         const blog = { title, body, author };
-        console.log(blog);
+
+        setIsPending(true);
+
+       // post request here as it only makes request once in whole application
+       //second argument here, tack on the data as well as define the type of request we are sending or a method 
+       setTimeout(() => {
+       fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json"}, //type of content were are sending with request
+            body: JSON.stringify(blog) //data being send, firstly changed into object from string
+        })
+        //promise
+        .then(() => {
+            console.log('new blog added');
+            setIsPending(false);
+        })}, 
+    1000);
     }
 
     return (  
         <div className="create">
             <h2>Add a New Blog</h2>
             <form onSubmit={handleSubmit}>
+
                 <label>Blog Title:</label>
                 <input 
                     type="text"
                     value= {title}
                     onChange={(e) => setTitle(e.target.value)}
                     required/>
+
                 <label>Blog Body:</label>
                 <textarea 
                     value= {body}
                     onChange={(e) => setBody(e.target.value)}
                     required></textarea>
+                     
                 <label>Blog Author:</label>
                 <select
                     value={author}
                     onChange={(e)=> setAuthor(e.target.value)}
                 >
-                    <option value="Mario">Mario</option>
-                    <option value="Yoshi">Yoshi</option>
+                    <option value="Mario">Anisha</option>
+                    <option value="Yoshi">Abhishekh</option>
                 </select>
-                <button>Add Blog</button>
+
+                { !isPending && <button>Add Blog</button> }
+                { isPending && <button disabled>Adding Blog...</button> }
             </form>
         </div>
     );
