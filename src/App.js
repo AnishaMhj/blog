b57e8da12860dc6c7c56019b2d60bd0d6fda4c74
anishmhrzn47 <anishamhrzn47@gmail.com>
2021-09-03 12:05:56 +1000

    add 404 page not found

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
 
@@ -22,6 +23,12 @@ function App() {
           </Route>
           <Route path="/blogs/:id">
             <BlogDetails/>
+          </Route>
+          {/* '*' ddenotes catch any routes and should always be placed at the bottom
+            checks the routes at the top if not matches, then matches this route
+          */}
+          <Route path="*">
+            <NotFound/>
           </Route>
         </Switch>
       </div>    
